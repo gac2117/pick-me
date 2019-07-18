36f822dca409be5562ae78cb60cd5288c1bb2ea0
Grace Choi <oiograceoio@gmail.com>
2019-07-18 16:39:12 -0400

    added validations to models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
 	has_many :tags	
+	validates :name, presence: true
+	validates :location, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
 	belongs_to :restaurant
+	validates :name, presence: true
 end
