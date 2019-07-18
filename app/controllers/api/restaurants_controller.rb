233afdd71a2b2260d27e57061681699146839052
Grace Choi <oiograceoio@gmail.com>
2019-07-18 13:34:32 -0400

    modified restaurants controller

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -19,7 +19,7 @@ class Api::RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
 
     if @restaurant.save
-      render json: @restaurant, status: :created, location: @restaurant
+      render json: @restaurant, status: :created
     else
       render json: @restaurant.errors, status: :unprocessable_entity
     end
